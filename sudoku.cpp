978c68a989cfdd574ce990ae5a652227337e2992
Nikola Leung <nikolaleung@gmail.com>
2019-10-15 22:15:58 -0700

    first commit

---
+++ sudoku.cpp
@@ -0,0 +1,14 @@
+// This program creates sudoku of various difficulty
+// Additionally it checks and allows tips during game play
+// By Nikola Leung
+
+#include <iostream>
+
+using namespace std;
+
+int main () {
+	
+	cout << "Let's play Sudoku" << endl;
+	
+	return 0;
+}
